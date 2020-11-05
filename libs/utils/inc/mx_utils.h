f106263ae05176aaa9e893039e3a5eeb82733199
Rostyslav Druzhchenko <rdruzhchenko@teamworkcommerce.com>
2020-11-05 19:47:18 +0200

    Add test lib

---
+++ libs/utils/inc/mx_utils.h
@@ -0,0 +1,12 @@
+//
+// Created by Rostyslav Druzhchenko on 05.11.2020.
+//
+
+#ifndef SERVER_MX_UTILS_H
+#define SERVER_MX_UTILS_H
+
+#include <stdio.h>
+
+void mx_print_about();
+
+#endif //SERVER_MX_UTILS_H
---
+++ libs/utils/src/mx_print_about.c
@@ -0,0 +1,9 @@
+//
+// Created by Rostyslav Druzhchenko on 05.11.2020.
+//
+
+#include <mx_utils.h>
+
+void mx_print_about() {
+    printf("(c) All rights reserved");
+}
