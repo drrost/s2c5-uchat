55d943258a832342bc21a6e86e602f00b6d210a5
Karyna Barinova <kbarinova@e3r3p4.unit.ua>
2020-11-11 09:58:07 +0200

    change color palette

--- client/src/main.c
+++ client/src/main.c
@@ -5,7 +5,7 @@
 #include <client.h>
 #include <mx_connection.h>
 
-static void show_signin_page(t_window_widgets *widgets) {
+static void show_signin_page(t_window_widgets *widgets) { //segfault
      gtk_widget_show(widgets->s_signin->login_window);
     // gtk_widget_hide(widgets->s_signup->signup_window);
      gtk_widget_hide(widgets->s_chat->window_main_chat);
@@ -57,13 +57,13 @@ void mx_init(t_info **info) {
     //mx_connect(*info);
 }
 
-void login_completion(e_connection_code code, t_response *response) {
-    if (code != E_CONNECTION_CODE_OK)
-        mx_printline("Connection error");
-    else if (response->code == E_STATUS_CODE_OK)
-        mx_printline("Logged in successfully");
-    mx_response_delete(&response);
-}
+// void login_completion(e_connection_code code, t_response *response) {
+//     if (code != E_CONNECTION_CODE_OK)
+//         mx_printline("Connection error");
+//     else if (response->code == E_STATUS_CODE_OK)
+//         mx_printline("Logged in successfully");
+//     mx_response_delete(&response);
+// }
 
 int main(int argc, char *argv[]) {
     t_info *info = 0;
@@ -72,14 +72,14 @@ int main(int argc, char *argv[]) {
     gtk_init(&argc, &argv);
     mx_init(&info);
 
-    t_connection *connection = mx_connection_open(info->ip, info->port);
-    t_request *request = mx_request_login("user", "password");
-    connection->send(connection, request, login_completion);
-    mx_request_delete(&request);
-    mx_connection_close(&connection);
+    // t_connection *connection = mx_connection_open(info->ip, info->port); //Connection is not established
+    // t_request *request = mx_request_login("user", "password");
+    // connection->send(connection, request, login_completion);
+    // mx_request_delete(&request);
+    // mx_connection_close(&connection);
     
     gtk_main();
-mx_show_window(info);
+    mx_show_window(info);
     mx_check_leaks();
 
     return EXIT_SUCCESS;
