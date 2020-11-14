661c7317b8611e85bfc515c58a360b4d77814df1
Rostyslav Druzhchenko <rdruzhchenko@teamworkcommerce.com>
2020-11-14 18:35:42 +0200

    Add a test server

---
+++ alt_server_2/src/main.c
@@ -0,0 +1,12 @@
+//
+// Created by Rostyslav Druzhchenko on 14.11.2020.
+//
+
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+    printf("Alternative server 2\n");
+    argc++;
+    argv++;
+    return 0;
+}
---
+++ alt_server_2/src/utils/utils.c
@@ -0,0 +1,8 @@
+//
+// Created by Rostyslav Druzhchenko on 14.11.2020.
+//
+
+void do_util(void ) {
+    int a = 0;
+    a++;
+}
