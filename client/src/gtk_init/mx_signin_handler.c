28b5a2f184ad5cffd08103fc99185a470bd891fc
Karyna Barinova <kbarinova@e3r4p1.unit.ua>
2020-11-18 11:45:46 +0200

    removed gtk_main() from mx_go_register

--- client/src/gtk_init/mx_signin_handler.c
+++ client/src/gtk_init/mx_signin_handler.c
@@ -59,7 +59,6 @@ static void mx_go_register(t_info *info) {
     gtk_widget_show(info->widgets->s_register->register_window);
     gtk_widget_hide(info->widgets->s_signin->login_window);
     gtk_widget_hide(info->widgets->s_chat_window->window_main_chat);
-    gtk_main();
 }
 
 void mx_signin_handler(t_info *info) {
