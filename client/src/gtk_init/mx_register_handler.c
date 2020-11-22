d589c3f20886d240911dcbab1e4066d1640d54d9
Karyna Barinova <kbarinova@e3r3p9.unit.ua>
2020-11-22 15:31:32 +0200

    added button signal handler

--- client/src/gtk_init/mx_register_handler.c
+++ client/src/gtk_init/mx_register_handler.c
@@ -16,7 +16,14 @@ t_info *register_info(t_info *in) {
     return info;
 }
 
+void mx_back(t_info *info) {
+    info = register_info(GET);
 
+    gtk_widget_show(info->widgets->s_signin->login_window);
+    gtk_widget_hide(info->widgets->s_register->register_window);
+    gtk_widget_hide(info->widgets->s_chat_window->window_main_chat);
+    mx_login_clear_input(info->widgets->s_signin);
+}
 
 void mx_go_to_login(t_info *info) {
     info = register_info(GET);
@@ -37,4 +44,6 @@ void mx_register_handler(t_info *info) {
                      G_CALLBACK(gtk_main_quit), NULL);
     g_signal_connect(GTK_WIDGET(regist->register_register_button), "clicked",
                      (GCallback)mx_go_to_login, info);
+    g_signal_connect(GTK_WIDGET(regist->register_back_button), "clicked",
+                     (GCallback)mx_back, info);
 }
