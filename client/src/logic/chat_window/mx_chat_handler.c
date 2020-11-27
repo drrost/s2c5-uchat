0e97bd3e3a014ceb3a7e7b28894b14c8f694a21e
Karyna Barinova <kbarinova@e1r11p7.unit.ua>
2020-11-27 18:41:12 +0200

    finished signal handler for refresh button

--- client/src/logic/chat_window/mx_chat_handler.c
+++ client/src/logic/chat_window/mx_chat_handler.c
@@ -61,16 +61,20 @@ void mx_message_selected( void) {
 
 void mx_expand_chat_history(void) {
     t_info *info = gs_info(GET);
+    char *id = 0;
     GtkListBoxRow *row = gtk_list_box_get_selected_row(
         GTK_LIST_BOX(info->widgets->s_chat_window->scrolled_chats_list));
     mx_clear_history(info);
-    char *id = g_object_get_data(G_OBJECT(row), "chat_id");
+    if (row)
+        id = g_object_get_data(G_OBJECT(row), "chat_id");
+    else
+        id = "1";
     info->user_info->chat_id = atoi(id);
     mx_run_message_list(info->token, info->user_info->chat_id);
 }
 
 void mx_refresh_message_list(void) {
-    printf("Refresh clicked\n");
+    mx_expand_chat_history();
 }
 
 void mx_chat_handler(t_info *info) {
